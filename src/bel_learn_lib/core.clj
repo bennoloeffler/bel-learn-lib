44ceaae4bcebdb633a2f5483e6d9f856f17a5a79
benno <benno.loeffler@gmx.de>
2020-11-03 21:51:54 +0100

    first commit

---
+++ src/bel_learn_lib/core.clj
@@ -0,0 +1,12 @@
+(ns bel-learn-lib.core
+  (:gen-class))
+
+
+(defn test-the-lib
+  "I just let a libs function to be called"
+  []
+  (println "clj-learn-lib.core/test-the-lib seems to work! Hello, World!")
+  "result is comming back... lib works!")
+
+(comment
+  (test-the-lib))
