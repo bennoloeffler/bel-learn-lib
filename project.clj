e9f3a9a721b4f210959d7bbe69330c19ce3ed8b5
Benno Löffler <benno.loeffler@gmx.de>
2020-12-20 17:00:56 +0100

    logging

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "WTFPL"
             :url "http://www.wtfpl.net/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.taoensso/timbre "5.1.0"] ; logging, see: https://github.com/ptaoussanis/timbre
                  [lein-koan "0.1.2"]]
   :main ^:skip-aot bel-learn-lib.core
   :target-path "target/%s"
