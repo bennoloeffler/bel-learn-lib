9e575e2e7d50c2d77c9b73e69eb3267e50132c81
benno <benno.loeffler@gmx.de>
2021-08-07 20:29:48 +0200

    example app

--- src/bel_learn_chapters/28_json.clj
+++ src/bel_learn_chapters/28_json.clj
@@ -1,5 +1,25 @@
-(ns bel-learn-chapters.28-json)
+(ns bel-learn-chapters.28-json
+  (:require [clojure.data.json :as json]))
 
 ; https://github.com/clojure/data.json
 
+(json/write-str {:a 1 :b 2})
+;;=> "{\"a\":1,\"b\":2}"
+
+(json/read-str "{\"a\":1,\"b\":2}")
+;;=> {"a" 1, "b" 2}
+
+(json/read-str "{\"a\":1,\"b\":2}"
+               :key-fn keyword)
+
+(def nested [ {:name "Benno"
+               :kids {:paul 19 :benno 22 :leo 14}}
+              {:name "Sabine"
+               :husband "Benno"
+               :age 48}])
+
+(def json-txt (json/write-str nested))
+
+(json/read-str json-txt
+               :key-fn keyword)
 
