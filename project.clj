428776bf9b5b383dbfb3e99b9d0b5e97daceb526
Benno Löffler <benno@vux.fritz.box>
2021-09-25 06:19:17 +0000

    test and cloverage

--- project.clj
+++ project.clj
@@ -39,6 +39,7 @@
   :target-path "target/%s"
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler bel-learn-chapters.50-http/handler}
+  :bat-test {:parallel? true :report [:pretty {:type :junit :output-to "target/junit.xml"}]} ; :report :progress
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
              :reveal {:dependencies [[nrepl,"0.8.3"][vlaaad/reveal "1.3.212"]]
@@ -49,6 +50,8 @@
                                   [nrepl,"0.8.3"]
                                   [vlaaad/reveal "1.3.212"]]
                    :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware]}
-                   :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                             [venantius/ultra "0.6.0"]]}})
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"] ; lein test-refresh
+                             [metosin/bat-test "0.4.4"] ; lein bat-test auto ;see eftest
+                             [lein-cloverage "1.2.2"]]}}) ; lein cloverage
+                             ;[venantius/ultra "0.6.0"];crashes with linux/idea2021.1]}})
 
