68215cddb82d8114a4687ffee1f470add946b70f
Benno Löffler <benno.loeffler@gmx.de>
2022-05-06 18:41:16 +0200

    require corrected

--- src/bel_learn_chapters/x_220_namespaces.clj
+++ src/bel_learn_chapters/x_220_namespaces.clj
@@ -1,10 +1,6 @@
-(ns bel-learn-chapters.x-220-namespaces
-  (require [core.async :as a :refer [!<]])
-  (:import [java.util ArrayList HashMap]))
+(ns bel-learn-chapters.x-220-namespaces)
 
 ;; https://gist.github.com/ghoseb/287710/
 
-(defn dummy []
-  (let[m (HasnMap.)]))
 
 
