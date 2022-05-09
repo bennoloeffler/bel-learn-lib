01c60c3d9f4efbb8134c6603a1472c2731176fb9
bennoloeffler <benno.loeffler@gmx.de>
2022-05-09 19:13:49 +0200

    book link

--- src/bel_learn_chapters/x_240_ddd_domain_data_driven_design.clj
+++ src/bel_learn_chapters/x_240_ddd_domain_data_driven_design.clj
@@ -12,21 +12,18 @@
 ; https://gist.github.com/maacl/b0795e5f3d4ab72ca8add1e2d091e0e4
 ; https://github.com/didibus/clj-ddd-example
 ; https://day8.github.io/re-frame/data-oriented-design/
+; https://ebin.pub/download/data-oriented-programming-unlearning-objects-version-2.html
 
+;; basic generators (local-date, simple-string)
 
-;; basic types
-(s/def ::date (s/with-gen #(instance? LocalDate %) #(s/gen (LocalDate/now))))
-(s/def ::simple-string (s/and string? #(re-matches #"[a-z0-9_-]+" %)))
-(s/def ::number number?)
-
-;; generator for ::simple-string
-(defn char-range [start end]
-  (map char (range (int start) (inc (int end)))))
-(def string-characters (-> [\- \_]
-                           (concat (range 0 10))
-                           (concat (char-range \a \z))
-                           (concat (char-range \A \Z))
-                           str/join))
+(defn gen-local-date
+  []
+  (gen/fmap
+    (fn [[y m d]]
+      (jt/local-date y m d))
+    (gen/tuple (gen/choose 1970 2050)
+               (gen/choose 1 13)
+               (gen/choose 1 28))))
 
 (defn rand-str
   "create random string with len len out of chars from string chars"
@@ -38,29 +35,49 @@
   len min-len up to max-len consisting of chars"
   [n min-len max-len chars]
   (let [min-to-max (inc ( - max-len min-len))]
-   (set (take n (repeatedly #(rand-str
-                               (+ min-len (rand-int min-to-max))
-                               chars))))))
+    (set (take n (repeatedly #(rand-str
+                                (+ min-len (rand-int min-to-max))
+                                chars))))))
+
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+(def string-characters (-> [\- \_]
+                           (concat (range 0 10))
+                           (concat (char-range \a \z))
+                           (concat (char-range \A \Z))
+                           str/join))
+
+(def simple-string-gen (gen/fmap (fn [_] (rand-str (inc (rand-int 33)) string-characters)) (s/gen int?)))
+
+;; basic types
+(s/def ::date (s/with-gen #(instance? LocalDate %) gen-local-date))
+(s/def ::simple-string (s/with-gen (s/and string? #(re-matches #"[a-z0-9_-]+" %)) simple-string-gen))
+(s/def ::number number?)
+
+(s/valid? ::simple-string "abc-1/")
+#_(gen/generate simple-string-gen)
+#_(gen/generate (s/gen ::simple-string))
+(gen/generate (s/gen ::date))
 
 
-(defn random-local-date []
-  (jt/local-date
-    (+ 1970 (rand-int 50))
-    (inc (rand-int 12))
-    (inc (rand-int 27))))
-(random-local-date)
-(def simple-local-date-gen (s/gen #(random-local-date)))
-(def simple-string-gen (s/gen (rand-str-set 33 1 30 string-characters)))
-(gen/sample simple-string-gen 5)
+#_(defn random-local-date []
+    (jt/local-date
+      (+ 1970 (rand-int 50))
+      (inc (rand-int 12))
+      (inc (rand-int 27))))
+#_(random-local-date)
+
+#_(def simple-local-date-gen (s/gen gen-local-date))
+#_(gen/sample simple-string-gen 5)
 
 
 ;; field types for task
-(s/def ::project-name (s/with-gen ::simple-string simple-string-gen))
+(s/def ::project-name ::simple-string)
 (s/def ::start-date ::date)
 (s/def ::end-date ::date)
-(s/def ::department (s/with-gen ::simple-string simple-string-gen))
+(s/def ::department ::simple-string)
 (s/def ::capacity-need ::number)
-(s/def ::comment (s/with-gen ::simple-string simple-string-gen))
+(s/def ::comment ::simple-string)
 
 (s/def ::task (s/keys :req [::project-name ::start-date ::end-date ::department ::capacity-need]
                       :opt [::comment]))
@@ -77,6 +94,6 @@
 (s/conform ::task task)
 (doc ::task)
 
-;(gen/generate (s/gen ::date))
+#_(gen/generate (s/gen ::simple-string))
 
 
