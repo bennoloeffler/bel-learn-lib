d0ef3a83b6110c6d34dff02fc9512410c6d58646
bennoloeffler <benno.loeffler@gmx.de>
2022-10-23 11:06:47 +0200

    rcf - tests in rich comments

--- src/bel_learn_chapters/09_tests_RICH.clj
+++ src/bel_learn_chapters/09_tests_RICH.clj
@@ -12,7 +12,15 @@
 (hyperfiddle.rcf/enable!)
 
 
-(defn square [x] (* x x))
+(defn square
+  "this is the test subject"
+  [x] (* x x))
 
 (tests
-  (square 6) := 36)
+  (square 6) := 36
+  (tests
+    81 := (square 9)
+    82) :<> (square 9))
+
+(tests
+   true := (number? 4))
